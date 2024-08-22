0634e35c3ca1f4ee53802faa06ee3d6cf9a1b069
Prabhat <prabhatku304@gmail.com>
2024-08-22 17:53:07 +0530

    hostel booking

--- src/util/quiz.ts
+++
@@ -1,9 +0,0 @@
-import { SPECIAL_QUIZ_IDS } from '../constants/constants';
-
-export const isSpecialQuiz = (quizId: string) => {
-  const specialQuizIds = SPECIAL_QUIZ_IDS.split(',');
-  if (specialQuizIds.includes(quizId)) {
-    return true;
-  }
-  return false;
-};
--- src/util/time.ts
+++
@@ -1,5 +0,0 @@
-export const delay = async (ms: number) => {
-  return new Promise(resolve => {
-    setTimeout(resolve, ms);
-  });
-};
